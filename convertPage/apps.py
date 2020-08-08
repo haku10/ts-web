7db53a1c4d3cd8c37032eca2104c1f122928c9e2
haku10 <isptry5689@gmail.com>
2020-08-09 02:05:17 +0900

    変換機能追加

--- convertPage/apps.py
+++ convertPage/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class ConvertpageConfig(AppConfig):
     name = 'convertPage'
--- convertPage/views.py
+++ convertPage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import FileResponse
+from convertPage.gcp_module import gcp 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -9,5 +11,13 @@ class IndexView(View):
         text = {
         'convertStr': request.POST.get('convertStr')
         }
+        # download_file = gcp.text_to_speech(text)
         return render(request, './convertPage/index.html', text)
 index = IndexView.as_view()
+
+class DownloadView(View):
+    def post(self, request, *args, **kwargs):
+        ctext = request.POST.get('converttext')
+        download_file = gcp.text_to_speech(ctext)
+        return download_file
+donwload = DownloadView.as_view()
