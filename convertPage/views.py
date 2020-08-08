41ee75ef6c078d1b37fbf3dd90dbc8e9edcb67bf
haku10 <isptry5689@gmail.com>
2020-08-08 15:33:45 +0900

    templateエンジン導入

--- convertPage/views.py
+++ convertPage/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("テストページへようこそ")
+    map = {
+        'hour': datetime.now().hour,
+        'message': 'Sample message',
+    }
+    return render(request, './convertPage/index.html' , map)
