91a119faef883c623edeb8908c87bf666f7cd29f
haku10 <isptry5689@gmail.com>
2020-08-08 16:09:26 +0900

    Get送信とPost送信の使い分け設定

--- convertPage/views.py
+++ convertPage/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
-from datetime import datetime
+from django.views.generic import View
 
-def index(request):
-    map = {
-        'hour': datetime.now().hour,
-        'message': 'Sample message',
-    }
-    return render(request, './convertPage/index.html' , map)
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, './convertPage/index.html')
+
+    def post(self, request, *args, **kwargs):
+        text = {
+        'convertStr': request.POST.get('convertStr')
+        }
+        return render(request, './convertPage/index.html', text)
+index = IndexView.as_view()
