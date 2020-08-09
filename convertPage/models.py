ce8eb71da843e0c3c83938bde85ef1963675677c
haku10 <isptry5689@gmail.com>
2020-08-09 10:10:34 +0900

    モデル追加&マイグレーションをできるように変更

--- convertPage/models.py
+++ convertPage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  id = models.AutoField(primary_key=True)
+  full_name = models.TextField(max_length=100)
+  email = models.TextField(max_length=100)
+
+  def __str__(self):
+    return self.full_name
