c9443a9fa64d1767bdb9b16ce04403a934e2ad6e
haku10 <isptry5689@gmail.com>
2020-11-15 20:46:47 +0900

    メニューバーの調整

--- convertPage/urls.py
+++ convertPage/urls.py
@@ -9,7 +9,8 @@ app_name = 'convertPage'
 urlpatterns = [
     path('login', auth_views.LoginView.as_view),
     path('', top.index, name='index'),
+    path('texttospeech', top.texttospeech, name='texttospeech'),
     path('download', top.donwload, name='donwload'),
     path('save', top.save, name='save'),
-    path('test', top.index, name='test'),
+    path('surmmarize', top.surmmarize, name='surmmarize'),
 ]
